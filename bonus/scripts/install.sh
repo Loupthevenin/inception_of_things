903208f1bdf907f2cefcef06f01b62a5d0a8bedb
Loupthevenin <loup.thevenin.1@gmail.com>
2025-07-18 15:50:32 +0200

    amd64

--- bonus/scripts/install.sh
+++ bonus/scripts/install.sh
@@ -36,7 +36,7 @@ fi
 # kubectl
 info "Installing kubectl..."
 if ! command -v kubectl &>/dev/null; then
-	curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
+	curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 	chmod +x kubectl
 	sudo mv kubectl /usr/local/bin
 	success "kubectl installed."
